f5bb841a51b8bca9eca1b61f448c6d410d4a7f29
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-09-18 14:19:18 -0600

    metodo guardarEnArchivo

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientodearchivosplanos/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientodearchivosplanos/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package mx.edu.ittepic.ladm_u1_practica2_almacenamientodearchivosplanos.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import mx.edu.ittepic.ladm_u1_practica2_almacenamientodearchivosplanos.databinding.FragmentHomeBinding
+import java.io.OutputStreamWriter
 
 class HomeFragment : Fragment() {
 
@@ -28,6 +31,18 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        binding.insertar.setOnClickListener {
+
+        }
+
+        fun guardarEnArchivo(mensaje:String) = try{
+            var archivo = OutputStreamWriter(activity?.openFileOutput("archivo.txt", Context.MODE_PRIVATE))
+            archivo.write(mensaje)
+            archivo.flush()
+            archivo.close()
+        }catch(e:Exception){
+            Toast.makeText(requireActivity(),e.message, Toast.LENGTH_LONG)
+        }
 
         return root
     }
