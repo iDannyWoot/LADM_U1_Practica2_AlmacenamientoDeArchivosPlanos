ca6ee8ca5332bc414544b6ae1b7cbed823b282f8
Chirstian Alexis <cralperezhe@ittepic.edu.mx>
2022-09-18 16:15:51 -0600

    se agregó el método leerDeArchivo.

--- app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientodearchivosplanos/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/mx/edu/ittepic/ladm_u1_practica2_almacenamientodearchivosplanos/ui/gallery/GalleryFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import mx.edu.ittepic.ladm_u1_practica2_almacenamientodearchivosplanos.databinding.FragmentGalleryBinding
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
 
 class GalleryFragment : Fragment() {
 
@@ -28,6 +32,25 @@ class GalleryFragment : Fragment() {
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        var datosPaciente = ""
+        fun leerDeArchivo(nombreArchivo: String) {
+            try{
+                var archivo = BufferedReader(InputStreamReader(requireActivity().openFileInput(nombreArchivo)))
+                datosPaciente = archivo.readLine()
+                archivo.close()
+            }catch (e:Exception){  }
+        }
+
+        var arrayDatos = datosPaciente.split("|")
+
+        var vector = ArrayList<String>()
+
+
+        for(i in arrayDatos) {
+            vector.add(i)
+        }
+
+        binding.lista.adapter = ArrayAdapter(requireActivity(),android.R.layout.simple_list_item_1, vector)
         
         return root
     }
